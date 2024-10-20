c1f3af3991c479f6bcecf846f40366fb24e901aa
Lior <liorsharony@gmail.com>
2024-10-20 16:26:08 +0300

    try

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,21 +1,13 @@
 'use client';
 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
-import {
-  CheckIcon,
-  ClockIcon,
-  CurrencyDollarIcon,
-  UserCircleIcon,
-} from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import { Button } from '@/app/ui/button';
+
 import { updateInvoice, State } from '@/app/lib/actions';
 import { useActionState } from 'react';
  
 
 export default function EditInvoiceForm({
-  invoice,
-  customers,
+  invoice
 }: {
   invoice: InvoiceForm;
   customers: CustomerField[];
